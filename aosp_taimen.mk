0ced4a55615666fc3eae08f9f7a309e02a58cc4b
Lunarixus <Nathanb644@hotmail.co.uk>
2020-06-04 16:00:49 +0000

    taimen: Fixup vendor inherits
    * Make vendors required as the ROM will not boot if they're not built
    * Include extra proprietary blobs to fix minor issues
    
    Signed-off-by: Lunarixus <Nathanb644@hotmail.co.uk>

--- aosp_taimen.mk
+++ aosp_taimen.mk
@@ -23,7 +23,8 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from device and vendor
 $(call inherit-product, device/google/taimen/device.mk)
-$(call inherit-product-if-exists, vendor/google_devices/taimen/proprietary/device-vendor.mk)
+$(call inherit-product, vendor/google_devices/taimen/proprietary/device-vendor.mk)
+$(call inherit-product, vendor/google_extras/taimen/taimen-vendor.mk)
 
 # Boot animation res
 TARGET_BOOT_ANIMATION_RES := 1440
